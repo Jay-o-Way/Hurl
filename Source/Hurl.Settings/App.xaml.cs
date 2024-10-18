c602117a545d959e53b314b712ffcb40b2337412
Jay <65828559+Jay-o-Way@users.noreply.github.com>
2024-10-18 17:12:46 +0200

    introduce Navigation

--- Source/Hurl.Settings/App.xaml.cs
+++ Source/Hurl.Settings/App.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.UI.Xaml;
+using Microsoft.UI.Xaml.Media.Animation;
 using System;
 using System.Diagnostics;
 using System.Text.Json;
@@ -39,39 +40,5 @@ public partial class App : Application
                                        System.Windows.MessageBoxButton.OK,
                                        System.Windows.MessageBoxImage.Error);
         Exit();
-    }
-
-    private MainWindow m_window;
-
-    protected override void OnLaunched(LaunchActivatedEventArgs args)
-    {
-        var cmdArgs = Environment.GetCommandLineArgs();
-
-        m_window = new MainWindow();
-
-        if (cmdArgs.Length > 1)
-        {
-            ProcessArgs(cmdArgs);
-        }
-        else
-        {
-            m_window.Activate();
-        }
-    }
-
-
-    void ProcessArgs(string[] args)
-    {
-        Debug.WriteLine(args[0]);
-        var primaryArg = args[1];
-
-        if (primaryArg.Equals("--page"))
-        {
-            if (args.Length > 2)
-            {
-                m_window.NavigateToPage(args[2]);
-                m_window.Activate();
-            }
-        }
     }
 }
--- Source/Hurl.Settings/MainWindow.xaml.cs
+++ Source/Hurl.Settings/MainWindow.xaml.cs
@@ -1,8 +1,14 @@
-using CommunityToolkit.WinUI;
+using Hurl.Settings.Views;
 using Microsoft.UI.Xaml;
 using Microsoft.UI.Xaml.Controls;
 using Microsoft.UI.Xaml.Media;
+using Microsoft.UI.Xaml.Media.Animation;
+using System;
+using System.Configuration;
+using System.Diagnostics;
+using System.Linq;
 using Windows.Graphics;
+using Windows.UI.Xaml.Navigation;
 
 namespace Hurl.Settings;
 
@@ -10,53 +16,152 @@ public sealed partial class MainWindow : Window
 {
     public MainWindow()
     {
-        this.InitializeComponent();
+        InitializeComponent();
 
         ExtendsContentIntoTitleBar = true;
-        Title = "Hurl SettingsPage Preview";
 
-        this.AppWindow.ResizeClient(new SizeInt32(1200, 840));
-        this.AppWindow.SetIcon("internet.ico");
-        this.SystemBackdrop = new MicaBackdrop();
-
-        NavigateToPage("browsers");
+        AppWindow.SetIcon("internet.ico");
+        //SystemBackdrop = new MicaBackdrop();
+        SystemBackdrop = new DesktopAcrylicBackdrop();
+        AppWindow.ResizeClient(new SizeInt32(720, 540));
+        // TO-DO: set minimum size
     }
 
-    private void OnNavItemClicked(object sender, ItemClickEventArgs e)
+
+    // Examples copied from https://learn.microsoft.com/windows/apps/design/controls/navigationview
+
+    private double NavViewCompactModeThresholdWidth { get { return NavView.CompactModeThresholdWidth; } }
+
+    private void ContentFrame_NavigationFailed(object sender, NavigationFailedEventArgs e)
     {
-        if (e.ClickedItem is FrameworkElement f && f.FindParent<ListViewItem>() is { Tag: string tag })
+        throw new Exception("Failed to load Page " + e.SourcePageType.FullName);
+    }
+
+    private void NavView_Loaded(object sender, RoutedEventArgs e)
+    {
+        // Add handler for ContentFrame navigation.
+        ContentFrame.Navigated += On_Navigated;
+
+        // NavView doesn't load any page by default, so load home page.
+        NavView.SelectedItem = NavView.MenuItems[0];
+
+        // If navigation occurs on SelectionChanged, this isn't needed.
+        // Because we use ItemInvoked to navigate, we need to call Navigate
+        // here to load the home page.
+        NavView_Navigate(typeof(BrowsersPage), new EntranceNavigationTransitionInfo());
+
+        // TO-DO: CMDARGS
+        //protected override void OnLaunched(LaunchActivatedEventArgs args)
+        //{
+        //    string[] cmdArgs = Environment.GetCommandLineArgs();
+        //    if (cmdArgs.Length > 1)
+        //    {
+        //    Debug.WriteLine(args[0]);
+        //    var primaryArg = args[1];
+
+        //    if (primaryArg.Equals("--page"))
+        //    {
+        //        if (args.Length > 2)
+        //        {
+        //            //mainwindow.NavView_Navigate(args[2], new EntranceNavigationTransitionInfo());
+        //        }
+        //    }
+        //    }
+        //}
+}
+
+private void On_Navigated(object sender, Microsoft.UI.Xaml.Navigation.NavigationEventArgs e)
+    {
+        throw new NotImplementedException();
+    }
+
+    private void NavView_ItemInvoked(NavigationView sender,
+                                     NavigationViewItemInvokedEventArgs args)
+    {
+        if (args.IsSettingsInvoked == true)
         {
-            NavigateToPage(tag);
+            NavView_Navigate(typeof(SettingsPage), args.RecommendedNavigationTransitionInfo);
+        }
+        else if (args.InvokedItemContainer != null)
+        {
+            Type navPageType = Type.GetType(args.InvokedItemContainer.Tag.ToString());
+            NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
         }
     }
 
-    public void NavigateToPage(string page)
+    // NavView_SelectionChanged is not used in this example, but is shown for completeness.
+    // You will typically handle either ItemInvoked or SelectionChanged to perform navigation,
+    // but not both.
+    private void NavView_SelectionChanged(NavigationView sender,
+                                          NavigationViewSelectionChangedEventArgs args)
     {
-        switch (page)
+        if (args.IsSettingsSelected == true)
         {
-            case "about":
-                NavigationFrame.Navigate(typeof(Views.AboutPage));
-                break;
-            case "browsers":
-                NavigationFrame.Navigate(typeof(Views.BrowsersPage));
-                //ViewModel.Navigate(ContentPageType.Apps);
-                break;
-            case "rulesets":
-                NavigationFrame.Navigate(typeof(Views.RulesetPage));
-                break;
-            case "settings":
-                NavigationFrame.Navigate(typeof(Views.SettingsPage));
-                break;
+            NavView_Navigate(typeof(SettingsPage), args.RecommendedNavigationTransitionInfo);
         }
+        else if (args.SelectedItemContainer != null)
+        {
+            Type navPageType = Type.GetType(args.SelectedItemContainer.Tag.ToString());
+            NavView_Navigate(navPageType, args.RecommendedNavigationTransitionInfo);
+        }
+    }
 
-        if (page == "about")
+    private void NavView_Navigate(
+        Type navPageType,
+        NavigationTransitionInfo transitionInfo)
+    {
+        // Get the page type before navigation so you can prevent duplicate
+        // entries in the backstack.
+        Type preNavPageType = ContentFrame.CurrentSourcePageType;
+
+        // Only navigate if the selected page isn't currently loaded.
+        if (navPageType is not null && !Type.Equals(preNavPageType, navPageType))
         {
-            NavMenuFooterList.SelectedIndex = 0;
-            NavMenuHeaderList.SelectedIndex = -1;
+            ContentFrame.Navigate(navPageType, null, transitionInfo);
         }
-        else
+    }
+
+    private void NavView_BackRequested(NavigationView sender,
+                                       NavigationViewBackRequestedEventArgs args)
+    {
+        TryGoBack();
+    }
+
+    private bool TryGoBack()
+    {
+        if (!ContentFrame.CanGoBack)
+            return false;
+
+        // Don't go back if the nav pane is overlayed.
+        if (NavView.IsPaneOpen &&
+            (NavView.DisplayMode == NavigationViewDisplayMode.Compact ||
+             NavView.DisplayMode == NavigationViewDisplayMode.Minimal))
+            return false;
+
+        ContentFrame.GoBack();
+        return true;
+    }
+
+    private void On_Navigated(object sender, NavigationEventArgs e)
+    {
+        NavView.IsBackEnabled = ContentFrame.CanGoBack;
+
+        if (ContentFrame.SourcePageType == typeof(SettingsBase))
         {
-            NavMenuFooterList.SelectedIndex = -1;
+            // SettingsItem is not part of NavView.MenuItems, and doesn't have a Tag.
+            NavView.SelectedItem = (NavigationViewItem)NavView.SettingsItem;
+            NavView.Header = "Settings";
+        }
+        else if (ContentFrame.SourcePageType != null)
+        {
+            // Select the nav view item that corresponds to the page being navigated to.
+            NavView.SelectedItem = NavView.MenuItems
+                        .OfType<NavigationViewItem>()
+                        .First(i => i.Tag.Equals(ContentFrame.SourcePageType.FullName.ToString()));
+
+            NavView.Header =
+                ((NavigationViewItem)NavView.SelectedItem)?.Content?.ToString();
+
         }
     }
 }
